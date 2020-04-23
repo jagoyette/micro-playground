713f6c585e795d7bca7811fb9a4ba55d85d3fa8e
John Goyette <John.Goyette@DentsplySirona.com>
2020-04-22 20:18:06 -0400

    Added logging

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 
 namespace PatientService.Controllers
 {
+    using Microsoft.Extensions.Logging;
     using Models;
     using Services;
 
@@ -11,23 +12,26 @@ namespace PatientService.Controllers
     [ApiController]
     public class PatientsController : ControllerBase
     {
+        private readonly ILogger _logger;
         private readonly PatientDataService _patientService;
 
-        public PatientsController(PatientDataService patientService)
+        public PatientsController(ILogger<PatientsController> logger, PatientDataService patientService)
         {
+            this._logger = logger;
             this._patientService = patientService;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Patient>>> GetPatients()
         {
+            _logger.LogDebug("API - GetPatients: Retrieving all patients");
             return await _patientService.Get();
         }
 
         [HttpPost]
         public async Task<ActionResult> CreatePatient(PatientInfo patientInfo)
         {
-
+            _logger.LogDebug($"API - CreatePatient: Creating new patient for {patientInfo.FirstName} {patientInfo.LastName}");
             return Ok(await _patientService.Create(patientInfo));
         }
     }
--- Services/PatientDataService.cs
+++ Services/PatientDataService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using MongoDB.Driver;
 using PatientService.Models;
 
@@ -7,10 +8,14 @@ namespace PatientService.Services
 {
     public class PatientDataService
     {
+        private readonly ILogger _logger;
         private readonly IMongoCollection<Patient> _patients;
 
-        public PatientDataService(IPatientDatabaseSettings settings)
+        public PatientDataService(ILogger<PatientDataService> logger, IPatientDatabaseSettings settings)
         {
+            _logger = logger;
+            _logger.LogInformation($"Creating Patient Data service with Connection: {settings.ConnectionString}");
+
             var client = new MongoClient(settings.ConnectionString);
             var database = client.GetDatabase(settings.DatabaseName);
 
@@ -33,7 +38,7 @@ namespace PatientService.Services
                 LastName = patientInfo.LastName,
                 PatientId = patientInfo.PatientId
             };
-
+            _logger.LogInformation($"Creating new patient with id: {patient.Uuid}");
             await _patients.InsertOneAsync(patient);
             return patient;
         }
