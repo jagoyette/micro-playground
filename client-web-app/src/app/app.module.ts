13ec936d97550e650bb7c3382fd51b75d3bfd3de
John Goyette <John.Goyette@DentsplySirona.com>
2020-04-24 21:58:51 -0400

    Added Angular Material

--- client-web-app/src/app/app.module.ts
+++ client-web-app/src/app/app.module.ts
@@ -1,9 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule, MatCardModule, MatButtonModule, MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
